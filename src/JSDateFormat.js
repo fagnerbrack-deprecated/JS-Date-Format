8a8b1def096b964c6bf4d4544347583e4584aee2
Fagner Martins <github@fagnermartins.com>
2012-03-17 17:33:43 -0300

    Reducing comments

--- src/JSDateFormat.js
+++ src/JSDateFormat.js
@@ -1,27 +1,9 @@
 /**
- * Copyright (c) 2012 Fagner Martins Brack, http://fagnerbrack.com/
+ * JS Date Format 1.0
+ * (c) 2010-2012 Fagner Martins Brack <fagnerbrack.com>
+ * MIT license
  * 
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- * 
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * 
- * === JSDateFormat ===
- * == Suporta:
+ * == Supports:
  * %d%d - Day of month (01, 02...20,21)
  * %M%M - Month of Year (03, 04...11, 12)
  * %y%y%y%y - Year (001990, 02008, 2009, 009, 09, 9)
