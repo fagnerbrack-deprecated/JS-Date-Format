add6d773aca2861407c6c3256550e0c6cd17e00b
Fagner Martins <github@fagnermartins.com>
2012-03-29 00:45:45 -0300

    Fix replaceAll bugs

--- src/utils.js
+++ src/utils.js
@@ -32,7 +32,7 @@ return str;
 };
 /**
  * ReplaceAll by Fagner Brack (MIT Licensed)
- * Replaces all occurences of a substring in a string
+ * Replaces all occurrences of a substring in a string
  */
 String.prototype.replaceAll = function(token, newToken, ignoreCase) {
 	var str, i = -1, _token;
@@ -40,7 +40,13 @@ String.prototype.replaceAll = function(token, newToken, ignoreCase) {
 		_token = ignoreCase === true? token.toLowerCase() : undefined;
 		while((i = (
 			_token !== undefined? 
-				str.toLowerCase().indexOf(_token, i + 1) : str.indexOf(token, i + 1)
+				str.toLowerCase().indexOf(
+							_token, 
+							i >= 0? i + newToken.length : 0
+				) : str.indexOf(
+							token,
+							i >= 0? i + newToken.length : 0
+				)
 		)) !== -1 ) {
 		    str = str.substring(0, i)
 		    		.concat(newToken)
